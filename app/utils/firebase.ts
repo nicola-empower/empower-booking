7ad5aaa17a437bd8aaa6663c9ad97f3ab9a5669f
Nicola Berry <nicola@empowervaservices.co.uk>
2025-12-07 16:24:43 +0000

    initial commit

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCgdurL6czdh_z2Okg4Ajjkb4Er9zqe7Ck",
+  authDomain: "nicola-booking.firebaseapp.com",
+  projectId: "nicola-booking",
+  storageBucket: "nicola-booking.firebasestorage.app",
+  messagingSenderId: "282156266094",
+  appId: "1:282156266094:web:01b834d3803e689a10ac7f",
+  measurementId: "G-NQCV5BW0YS"
 };
 
 // Initialize Firebase
@@ -18,4 +19,13 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
+// Analytics (only on client)
+let analytics;
+if (typeof window !== "undefined") {
+  import('firebase/analytics').then(({ getAnalytics }) => {
+    analytics = getAnalytics(app);
+  });
+}
+
+export { analytics };
 export default app;
